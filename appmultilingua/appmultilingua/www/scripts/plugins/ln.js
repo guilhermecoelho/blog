f9ff34d9bfc14805c705744bf0aeccdd03449021
guilhermecoelho <guilhermecoelho2@gmail.com>
2016-03-26 19:05:50 -0300

    multi-linguagem app

---
+++ appmultilingua/appmultilingua/www/scripts/plugins/ln.js
@@ -0,0 +1,20 @@
+﻿var ln =
+{
+    init: function () {
+
+        i18n.init
+        ({
+            lng: 'en',
+            ns: 'general',
+            useCookie: false,
+            fallbackLng: 'en',
+            resGetPath: 'locales/__ns__.__lng__.json'
+        }, function () {
+            $('body').i18n();
+
+        });
+    },
+
+};
+
+ln.init();
